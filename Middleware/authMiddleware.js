10ea60eb0e3525fb210c9a4663eed536eaf105c9
bcincioglu <baris.cincioglu@outlook.com>
2024-08-17 22:17:17 +0300

    last version

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -12,7 +12,9 @@ exports.isAuth = (req, res, next) => {
     if (err) {
       return res.status(401).json({ message: 'Erişim reddedildi: Geçersiz yetkilendirme tokeni' });
     }
+    console.log(decodedToken);
     req.user = decodedToken; // decodedToken içinde kullanıcı bilgileri varsa, bu bilgileri request nesnesine ekleyebilirsiniz
+    console.log(req.user.id); // bu user id
     next();
   });
 };
